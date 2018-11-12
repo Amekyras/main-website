5bb57935959047c49cbba2377034e58daa1291b6
Matthew Grove <matthew@grove.gq>
2018-11-12 20:07:43 +0000

    Fix typo and move some code around

--- demo/assets/global.js
+++ demo/assets/global.js
@@ -3,20 +3,10 @@ $(document).ready(function(){
     $(".navbar-insert").load("/demo/page-inserts/navbar.html", function(){
         // initiate MDC drawer
         const drawer = new mdc.drawer.MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
-        
-        // initiate MDC buttons & FABs
-        const mdc-buttons = document.querySelectorAll('.mdc-button');
-        for (const button of mdc-buttons) {
-            mdc.ripple.MDCRipple.attachTo(button);
-        }
-        const mdc-fabs = document.querySelectorAll('.mdc-fab');
-        for (const fab of mdc-fabs) {
-            mdc.ripple.MDCRipple.attachTo(fab);
-        }
     });
-    
+
     // include top app bar
-    $(".top-app-bar-insert").load("/demo/page-inserts/top-app-bar.html", function(){
+    $(".mdc-top-app-bar").load("/demo/page-inserts/top-app-bar.html", function(){
         // initiate MDC top app bar
         const mdc-top-app-bar = new mdc.topAppBar.MDCTopAppBar.attachTo(document.querySelector('.mdc-top-app-bar'));
         topAppBar.setScrollTarget(document.getElementById('main-content'));
@@ -24,7 +14,17 @@ $(document).ready(function(){
             drawer.open = !drawer.open;
         });
     });
-    
+
     // include source code link
-    $(".source-code-link").load("/demo/page-inserts/source-code-link.html");
+    $(".source-code-link").load("/demo/page-inserts/source-code-link.html", function(){alert("Done!");});
+
+    // initiate MDC buttons & FABs
+    const mdc-buttons = document.querySelectorAll('.mdc-button');
+    for (const button of mdc-buttons) {
+        mdc.ripple.MDCRipple.attachTo(button);
+    }
+    const mdc-fabs = document.querySelectorAll('.mdc-fab');
+    for (const fab of mdc-fabs) {
+        mdc.ripple.MDCRipple.attachTo(fab);
+    }
 });
