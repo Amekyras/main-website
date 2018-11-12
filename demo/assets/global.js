1ad9ceacf90eab443d18cb056c055cb684bb4635
Matthew Grove <matthew@grove.gq>
2018-11-12 20:55:31 +0000

    Hard code top app bar (remove insert via jQuery)

--- demo/assets/global.js
+++ demo/assets/global.js
@@ -3,20 +3,17 @@ $(document).ready(function(){
     $(".navbar-insert").load("/demo/page-inserts/navbar.html", function(){
         // initiate MDC drawer
         const drawer = new mdc.drawer.MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
-        
-        // include top app bar
-        $(".mdc-top-app-bar").load("/demo/page-inserts/top-app-bar.html", function(){
-            // initiate MDC top app bar
-            const mdc_top_app_bar = new mdc.topAppBar.MDCTopAppBar.attachTo(document.querySelector('.mdc-top-app-bar'));
-            mdc_top_app_bar.setScrollTarget(document.getElementById('main-content'));
-            mdc_top_app_bar.listen('MDCTopAppBar:nav', () => {
-                drawer.open = !drawer.open;
-            });
+
+        // initiate MDC top app bar
+        const mdc_top_app_bar = new mdc.topAppBar.MDCTopAppBar.attachTo(document.querySelector('.mdc-top-app-bar'));
+        mdc_top_app_bar.setScrollTarget(document.getElementById('main-content'));
+        mdc_top_app_bar.listen('MDCTopAppBar:nav', () => {
+            drawer.open = !drawer.open;
         });
-    
+
         // include source code link
         $(".source-code-link").load("/demo/page-inserts/source-code-link.html");
-    
+
         // initiate MDC buttons, FABs and text fields
         const mdc_buttons = document.querySelectorAll('.mdc-button');
         for (const button of mdc_buttons) {
